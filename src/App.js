55de06b8499abcc7ed3d59c6718a78025404f1e8
brktopcu <buraktopcu12@hotmail.com>
2019-09-28 22:44:47 +0300

    foto4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
+import ogz from "./1569698098493.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./1569698098493.svg" className="App-logo" alt="logo" />
+        <img src={ogz} className="App-logo" alt="logo" />
         <p>Merzifon forever !!!!</p>
       </header>
     </div>
